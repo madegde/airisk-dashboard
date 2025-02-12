7b76a1e0bad7e2c7c709cd2c0a3426c15d047bc5
madegde <73327109+madegde@users.noreply.github.com>
2025-02-12 19:38:47 +0000

    Update streamlit_app.py
    add import subplot

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import math
 from pathlib import Path
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
